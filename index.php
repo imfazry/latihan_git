b426ad894b52ea6cfb6f9dff5088d9c124d270eb
root <root@DESKTOP-RS9GV17.localdomain>
2022-08-10 23:20:48 +0700

    initial push

---
+++ index.php
@@ -0,0 +1,242 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Proteksi Usaha Indonesia</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">    
+    
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/jquery-ui.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="shortcut icon" type="images/png" href="/icon.ico"/>
+
+    <link rel="stylesheet" href="css/jquery.fancybox.min.css">
+
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+
+    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+
+    <link rel="stylesheet" href="css/aos.css">
+
+    <link rel="stylesheet" href="css/style.css">
+    <link href="images/Icon_proteksi.ico" rel="shortcut icon">
+    
+  </head>
+  <body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
+  
+
+  <div id="overlayer"></div>
+  <div class="loader">
+    <div class="spinner-border text-primary" role="status">
+      <span class="sr-only">Loading...</span>
+    </div>
+  </div>
+
+
+  <div class="site-wrap">
+
+    <div class="site-mobile-menu site-navbar-target">
+      <div class="site-mobile-menu-header">
+        <div class="site-mobile-menu-close mt-3">
+          <span class="icon-close2 js-menu-toggle"></span>
+        </div>
+      </div>
+      <div class="site-mobile-menu-body"></div>
+    </div>
+    <header class="site-navbar js-sticky-header site-navbar-target" role="banner">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-6 col-xl-6">
+            <h6 class="mb-0 site-logo">
+              <img src="images/logo_proteksi.png" alt="Smiley face" height="30" width="30">
+              <a href="index.html" class="h6 mb-0"><b>Proteksi Usaha Indonesia</b><span class="text-primary">.</span></a>
+            </h6>
+          </div>
+          <div class="col-6 col-md-6 d-none d-xl-block">
+            <nav class="site-navigation position-relative text-right" role="navigation">
+
+              <ul class="site-menu main-menu js-clone-nav mr-auto d-none d-lg-block">
+                <li><a href="#home-section" class="nav-link">Home</a></li>
+                <li><a href="#about-section" class="nav-link">About Us</a></li>
+                <!-- <li class="has-children">
+                  <a href="#about-section" class="nav-link">About Us</a>
+                  <ul class="dropdown">
+                    <li><a href="#team-section" class="nav-link">Team</a></li>
+                    <li><a href="#pricing-section" class="nav-link">Pricing</a></li>
+                    <li><a href="#faq-section" class="nav-link">FAQ</a></li>
+                    <li><a href="#gallery-section" class="nav-link">Gallery</a></li>
+                    <li><a href="#services-section" class="nav-link">Services</a></li>
+                    <li><a href="#testimonials-section" class="nav-link">Testimonials</a></li>
+                    <li class="has-children">
+                      <a href="#">More Links</a>
+                      <ul class="dropdown">
+                        <li><a href="#">Menu One</a></li>
+                        <li><a href="#">Menu Two</a></li>
+                        <li><a href="#">Menu Three</a></li>
+                      </ul>
+                    </li>
+                  </ul>
+                </li> -->
+                
+                
+                <!-- <li><a href="#blog-section" class="nav-link">Blog</a></li> -->
+                <li><a href="#contact-section" class="nav-link">Contact</a></li>
+              </ul>
+            </nav>
+          </div>
+          <div class="col-6 d-inline-block d-xl-none ml-md-0 py-3" style="position: relative; top: 3px;"><a href="#" class="site-menu-toggle js-menu-toggle float-right"><span class="icon-menu h3"></span></a></div>
+
+        </div>
+      </div>
+      
+    </header>
+     
+    <div class="site-blocks-cover overlay" style="background-image: url(images/bckg_seal.jpg);" data-aos="fade" id="home-section">
+      <div class="container">
+        <div class="row align-items-center justify-content-center">
+          <div class="col-md-10 mt-lg-5 text-center">
+            <div class="single-text owl-carousel">
+              <div class="slide">
+                <h1 class="text-uppercase" data-aos="fade-up"></h3>
+                <h1 class="text-uppercase" data-aos="fade-up">Cargo Tracking</h1>
+                 <p class="mb-5 desc"  data-aos="fade-up" data-aos-delay="100">Providing safety and security for cargos, trucks and driver</p>
+                 <p class="mb-5 desc"  data-aos="fade-up" data-aos-delay="100">Coming Soon</p>
+                <div data-aos="fade-up" data-aos-delay="100">
+                </div>
+              </div>
+
+              <div class="slide">
+                <h1 class="text-uppercase" data-aos="fade-up">Vehicle Tracking</h1>
+                 <p class="mb-5 desc"  data-aos="fade-up" data-aos-delay="100">Automatic theft or unauthorized uses notification</p>
+                 <p class="mb-5 desc"  data-aos="fade-up" data-aos-delay="100">Coming Soon</p>
+              </div>
+            </div>
+            <p class="lead" data-aos="fade-up" data-aos-delay="100"><font size="5"><b>Our website is under construction.</b></font></p>
+            <p class="lead" data-aos="fade-up" data-aos-delay="100"><font size="5"><b>We`ll be here soon with our new awesome site</b></font></p>
+          </div>    
+        </div>
+      </div>
+    </div>
+
+    <div class="site-section cta-big-image" id="about-section">
+      <div class="container">
+        <div class="row mb-5 justify-content-center">
+          <div class="col-md-8 text-center">
+            <h2 class="section-title mb-3" data-aos="fade-up" data-aos-delay="">About Us</h2>
+            <p class="lead" data-aos="fade-up" data-aos-delay="100">PT. Proteksi  Usaha  Indonesia,  is a joint venture between PT. Cikarang Inland Port  dan Ascent Solutions Pte Ltd. PT. Cikarang Inland Port manages and operates Cikarang Dry Port, an integrated port and logistics facilities in Cikarang Indonesia. Ascent Solutions is a Singapore-based IoT company that specializes in cargo security and track & trace for smart logistics. Both companies had more than 8 years experience in developing smart logistics solution in Cikarang Dry Port for terminal operations and its bonded logistics center.</p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-lg-6 mb-5" data-aos="fade-up" data-aos-delay="">
+            <figure class="circle-bg">
+            <img src="images/bckg_seal.jpg" alt="Free Website Template by Free-Template.co" class="img-fluid">
+            </figure>
+          </div>
+          <div class="col-lg-5 ml-auto" data-aos="fade-up" data-aos-delay="100">    
+            <h3 class="text-black mb-4">Cargo Tracking</h3>
+            <ul>
+            <li><p>Providing safety and security for cargos, trucks and driver</p></li>
+            <li><p>Providing more value in services for customers of logistics services</p></li>
+            <li><p>Providing data to improve fleet operation and time management</p></li>
+            </ul>     
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-lg-6 mb-5" data-aos="fade-up" data-aos-delay="">
+            <figure class="circle-bg">
+            <img src="images/bck01.jpg" alt="Free Website Template by Free-Template.co" class="img-fluid">
+            </figure>
+          </div>
+          <div class="col-lg-5 ml-auto" data-aos="fade-up" data-aos-delay="100">    
+            <h3 class="text-black mb-4">Vehicle Tracking</h3>
+            <ul>
+            <li><p>Automatic theft or unauthorized uses notification</p></li>
+            <li><p>Reducing administrative and paper works hassle</p></li>
+            <li><p>Optimizing vehicle assets utilization</p></li>
+            </ul>     
+          </div>
+        </div>            
+      </div>  
+    </div>
+    <section class="site-section bg-light" id="contact-section" data-aos="fade">
+      <div class="container">
+        <div class="row mb-5">
+          <div class="col-12 text-center">
+            <h2 class="section-title mb-3">Contact Us</h2>
+          </div>
+        </div>
+        <div class="row mb-5">
+          <div class="col-md-4 text-center">
+            <p class="mb-4">
+              <span class="icon-room d-block h2 text-primary"></span>
+              <a>Jalan Dry Port Raya Kota Jababeka Cikarang Kabupaten Bekasi 17530, Indonesia</a>
+            </p>
+          </div>
+          <div class="col-md-4 text-center">
+            <p class="mb-4">
+              <span class="icon-phone d-block h2 text-primary"></span>
+              <a href="#">(021) 29082908</a>
+            </p>
+          </div>
+          <div class="col-md-4 text-center">
+            <p class="mb-0">
+              <span class="icon-mail_outline d-block h2 text-primary"></span>
+              <a href="#">marketing@proteksiui.com</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <footer class="site-footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-9">
+            <div class="row">
+              <table border="0">
+                <tr>
+                <td colspan="4">
+                  <p align="center"><b><a>Affiliations</a></b></p>
+                </td>
+                </tr>  
+                <tr>
+                <td>
+                  <a href="http://www.myascents.com"><img src="images/ascent_logo.png" alt="Smiley face"></a>
+                </td>
+                <td>&nbsp;&nbsp;&nbsp;</td>
+                <td>&nbsp;&nbsp;&nbsp;</td>
+                <td>
+                  <a href="http://cikarangdryport.com"><img src="images/cdp_logo.png" alt="Smiley face" height="50" width="150"></a>
+                </td>
+                </tr>  
+              </table>
+            </div>
+            <div class="row">
+            </div>
+          </div>
+      </div>
+    </footer>
+
+  </div> <!-- .site-wrap -->
+
+  <script src="js/jquery-3.3.1.min.js"></script>
+  <script src="js/jquery-ui.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.countdown.min.js"></script>
+  <script src="js/jquery.easing.1.3.js"></script>
+  <script src="js/aos.js"></script>
+  <script src="js/jquery.fancybox.min.js"></script>
+  <script src="js/jquery.sticky.js"></script>
+  <script src="js/isotope.pkgd.min.js"></script>
+
+  
+  <script src="js/main.js"></script>
+
+  
+  </body>
+</html>
